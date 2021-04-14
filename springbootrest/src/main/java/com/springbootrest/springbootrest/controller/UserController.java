43087f384722227502e6ca501c135834c6826368
HP <HP@192.168.1.5>
2021-04-14 09:47:43 +0530

    New update

--- springbootrest/src/main/java/com/springbootrest/springbootrest/controller/UserController.java
+++ springbootrest/src/main/java/com/springbootrest/springbootrest/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.springbootrest.springbootrest.controller;
 
 
+import java.security.Principal;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,70 +37,37 @@ public class UserController {
 	@Autowired
     private UserService userService;
 	
-//	@GetMapping("/helloworld")
-//	public String HelloWorld() {
-//		return "Hello World";
-//	}
-//	
-//	@GetMapping("/allusers")
-//	public List<Users>getUsers(){
-//		return this.userService.getUsers();
-//	}
-//	@GetMapping("/register")
-//	public String showForm(Model model){
-//		Users users = new Users();
-//		model.addAttribute("user",users);
-//		return "register_form";
-//	}
-//	@RequestMapping(value="/register",method =RequestMethod.POST)
-//	public String doRegistration(@ModelAttribute("user")Users users){
-//	System.out.println(users);
-//	
-//
-//	return "register_success";
-//}
-//	
-	@RequestMapping(path="/{name}",method = RequestMethod.GET)
-    public String getMessage(@PathVariable(required = false) String name) {
-        
-        
-        
-        return name;
-    }
-	
-	
-	@RequestMapping(value = "/modules/**", method = RequestMethod.GET)
-	@ResponseBody
-	public String moduleStrings(HttpServletRequest request) {
-
-	    String requestURL = request.getRequestURL().toString();
-	    System.out.println(requestURL);
-
-	    String moduleName = requestURL.split("/modules/")[1];
-	    System.out.println(moduleName);
-	    return moduleName;
-
+	@GetMapping("/")
+	public String HelloWorld() {
+		return "Hello World";
 	}
-	@GetMapping(path = {"/user/**", "/user/{data}"})
-	public String user(@PathVariable(required=false,name="data") String data,
-	                 @RequestParam(required=false) Map<String,String> qparams) {
-		String result="";
-	    	  Iterator <Map.Entry<String, String>> itr = qparams.entrySet().iterator();
-	    	  while(itr.hasNext())
-	          {
-	    		  System.out.print("Inside this method");
-	               Map.Entry<String, String> entry = itr.next();
-	             result += entry.getKey() + entry.getValue();
-	          }
-	    	  
-	    if (data != null) {
-	        System.out.println(data);
-	        return data;
-	    }
-	    return result;
+
+	
+	@GetMapping("/home")
+	public String restricted(Principal principal) {
+		return principal.getName();
 	}
 	
+
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 	
 }
